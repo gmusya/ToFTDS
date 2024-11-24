fa6eadbb2b25f69d2d56e17e975e5edddd6142b4
German Perov <ggmusya@gmail.com>
2024-11-24 21:05:45 +0300

    Add test for integral

--- hw1/src/common/integral.cpp
+++ hw1/src/common/integral.cpp
@@ -3,12 +3,12 @@
 
 namespace integral {
 
-double EvaluateIntegral(double a, double b, uint32_t points,
+double EvaluateIntegral(const double a, const double b, const uint32_t points,
                         const Function &function) {
   ENSURE(a < b);
 
   double result_raw = 0;
-  double step_length = (b - a) / points;
+  const double step_length = (b - a) / points;
   for (double x = a + step_length / 2; x < b; x += step_length) {
     result_raw += function(x);
   }
--- hw1/src/common/integral.h
+++ hw1/src/common/integral.h
@@ -1,12 +1,13 @@
 #pragma once
 
-#include <functional>
 #include <cstdint>
+#include <functional>
 
 namespace integral {
 
 using Function = std::function<double(double)>;
 
-double EvaluateIntegral(double a, double b, uint32_t points);
+double EvaluateIntegral(double a, double b, uint32_t points,
+                        const Function &function);
 
-}
+} // namespace integral
